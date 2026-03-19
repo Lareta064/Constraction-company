bb76cac4696c2b2e12f5a2d458f8ace593a10869
Lareta064 <lareta1@yandex.ru>
2026-03-20 01:02:04 +0400

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const bodyEl = document.body
+	const bodyEl = document.body;
+	const transformCards = document.querySelectorAll('.transform-card');
+	if(transformCards.length>0){
+		transformCards.forEach((item) =>{
+			const itemButton = item.querySelector('.transform-card-btn');
+			const itemContent = item.querySelector('.transform-card-content');
+			
+			itemButton.addEventListener('click', ()=>{
+				if(item.classList.contains('is-open')){
+					item.classList.remove('is-open');
+					if(window.innerWidth > 767){
+						itemContent.style.height = '51px';
+					}else{
+						itemContent.style.height = '136px';
+					}
+					
+				}else{
+					item.classList.add('is-open');
+					itemContent.style.height = itemContent.scrollHeight + 'px';
+				}
+			})
+		});
+	}
 })
